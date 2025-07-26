da79552ee2d83ee0dcc7732a21acb9a6cbad969f
Lyxbux <lyxbux@proton.me>
2025-07-26 14:22:27 +0530

    ServiceRegistry added.

---
+++ Lyxbux.Mvvm/ServiceRegistry.cs
@@ -0,0 +1,25 @@
+﻿using Lyxbux.Registry;
+
+namespace Lyxbux.Mvvm
+{
+    public static class ServiceRegistry
+    {
+        public static bool RegisterService(string serviceName, IService service)
+        {
+            return AppRegistry.Register($@"Services/{serviceName}", service);
+        }
+        public static bool UnregisterService(string serviceName)
+        {
+            return AppRegistry.Unregister($@"Services/{serviceName}");
+        }
+        public static IService? GetService(string serviceName)
+        {
+            if (AppRegistry.Get($@"Services/{serviceName}", out IService? service))
+            {
+                return service;
+            }
+
+            return null;
+        }
+    }
+}
